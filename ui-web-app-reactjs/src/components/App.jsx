df6c173245871f8be3277bb1563287cf46a93af7
chandan kumar <128124398+chandanone@users.noreply.github.com>
2025-02-17 00:26:10 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://54.242.164.15:9999/'+e.target.name;
+    const url = 'http://54.82.117.190:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
